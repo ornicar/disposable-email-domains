5a0981902fdecb390501ad3d1d4e7e941c07ec15
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-05-31 09:24:19 +0200

    allow a domain

--- sync/safeDomains.js
+++ sync/safeDomains.js
@@ -40,7 +40,10 @@ var domains = [
   "hotmail.com", "gmail.com", "yahoo.com.mx", "live.com.mx", "yahoo.com", "hotmail.es", "live.com", "hotmail.com.mx", "prodigy.net.mx", "msn.com",
 
   /* Domains used in Brazil */
-  "yahoo.com.br", "hotmail.com.br", "outlook.com.br", "uol.com.br", "bol.com.br", "terra.com.br", "ig.com.br", "itelefonica.com.br", "r7.com", "zipmail.com.br", "globo.com", "globomail.com", "oi.com.br"
+  "yahoo.com.br", "hotmail.com.br", "outlook.com.br", "uol.com.br", "bol.com.br", "terra.com.br", "ig.com.br", "itelefonica.com.br", "r7.com", "zipmail.com.br", "globo.com", "globomail.com", "oi.com.br",
+
+  /* meh */
+  "poczta.onet.pl"
 ];
 
 module.exports = {
