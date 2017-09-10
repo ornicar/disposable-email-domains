8b2e2406d7ca56be0a57e3c134ee8902c8169083
Thibault Duplessis <thibault.duplessis@gmail.com>
2017-09-10 15:50:34 -0500

    tweak batch insert

--- sync/bunch.js
+++ sync/bunch.js
@@ -1,3 +1,10 @@
 var storage = require('./storage');
 
-process.stdin.pipe(require('split')()).on('data', storage.addDomain);
+// process.stdin.pipe(require('split')()).on('data', storage.addDomain);
+
+var domains = [
+];
+
+domains.reduce((p, domain) => {
+  return p.then(() => storage.addDomain(domain));
+}, Promise.resolve());
